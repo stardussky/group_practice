6972381c79f102cd45c9a340043ef5662680c517
stardussky <stardussky@gmail.com>
2020-01-22 16:12:18 +0800

    update

--- public/dd104g3/member/getMessage.php
+++ public/dd104g3/member/getMessage.php
@@ -3,7 +3,7 @@
     require_once('../pdo.php');
     $sql = 'select j.pro_no, m.mem_id, m.mem_name, m.headshot, p.pro_title from `join_program` j
     JOIN `member` m on m.mem_no = j.inv_by_mem
-    JOIN `program` p on p.mem_no = m.mem_no AND p.pro_no = j.pro_no
+    JOIN `program` p on p.pro_no = j.pro_no
     WHERE j.mem_no = :mem_no AND pro_mem_inv = 0 AND inv_sta = 0';
     $res = $pdo->prepare($sql);
     $res->bindParam('mem_no', $_POST['mem_no']);
